8d3276661492df488e88bed028f10c966f53e32d
fedor <fedor.malyshkin@yandex.ru>
2017-04-05 23:24:08 +0300

    add base methods

--- src/main/java/ru/nlp_project/story_line2/server_web/impl/StormDRPCClientImpl.java
+++ src/main/java/ru/nlp_project/story_line2/server_web/impl/StormDRPCClientImpl.java
@@ -6,6 +6,8 @@ import java.util.Map;
 
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.storm.thrift.TException;
 import org.apache.storm.thrift.transport.TTransportException;
 import org.apache.storm.utils.DRPCClient;
@@ -54,12 +56,14 @@ public class StormDRPCClientImpl implements IStormDRPCClient {
 		Map<String, Object> args = new HashMap<String, Object>();
 		args.put("source", source);
 		args.put("count", count);
+		String drpcResult;
 		try {
-			return client.execute(DRPC_METHOD_GET_NEWS_HEADERS, JSONUtils.serialize(args));
+			drpcResult = client.execute(DRPC_METHOD_GET_NEWS_HEADERS, JSONUtils.serialize(args));
 		} catch (TException e) {
 			log.error(e.getMessage(), e);
 			throw new IllegalStateException(e);
 		}
+		return removeSurroundingBrackets(drpcResult);
 	}
 
 	private DRPCClient getClient() {
@@ -74,14 +78,22 @@ public class StormDRPCClientImpl implements IStormDRPCClient {
 		// args
 		Map<String, Object> args = new HashMap<String, Object>();
 		args.put("id", id);
+		String drpcResult;
 		try {
-			return client.execute(DRPC_METHOD_GET_NEWS_ARTICLE, JSONUtils.serialize(args));
+			drpcResult = client.execute(DRPC_METHOD_GET_NEWS_ARTICLE, JSONUtils.serialize(args));
 		} catch (TException e) {
 			log.error(e.getMessage(), e);
 			throw new IllegalStateException(e);
 		}
+		return removeSurroundingBrackets(drpcResult);
 	}
 
 
+	private String removeSurroundingBrackets(String execute) {
+		String res = StringUtils.removeStartIgnoreCase(execute, "[[\"");
+		res = StringUtils.removeEndIgnoreCase(res, "\"]]");
+		res = StringEscapeUtils.unescapeJson(res);
+		return res;
+	}
 
 }
